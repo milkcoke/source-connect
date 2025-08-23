b9b4b066f419d49cbee3840d010f71009a209b74
milkcoke <mbh023@gmail.com>
2025-08-24 00:21:18 +0900

    refactor: @TestInstance Lifecycle PER_CLASS 적용
    - @BeforeAll 어노테이션을 통해 테스트용 오프셋 토픽 생성 코드를 추가.
    - TestInstance 객체가 매번 생성될 필요 없이 하나의 객체만 생성되면 됨 (PER_CLASS)
    - 테스트 토픽명, S3OffsetRepository, Properties 가 공유되도 상관 없기 때문이다.

--- s3-source-connector/src/test/java/sourceconnector/repository/S3OffsetRepositoryTest.java
+++ s3-source-connector/src/test/java/sourceconnector/repository/S3OffsetRepositoryTest.java
@@ -9,11 +9,9 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.serialization.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.kafka.config.TopicBuilder;
 import sourceconnector.domain.OffsetRecord;
 import sourceconnector.domain.OffsetStatus;
@@ -27,34 +25,36 @@ import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class S3OffsetRepositoryTest {
-  private static S3OffsetRepository repository;
+  private final String testTopicName = "offset-topic";
+  private S3OffsetRepository repository;
   private static final Properties props = new Properties();
   static {
     props.putAll(Map.of(
         CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
-        org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG, "-1",
-        org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
-        org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class,
-        org.apache.kafka.clients.producer.ProducerConfig.LINGER_MS_CONFIG, 100,
+        ProducerConfig.ACKS_CONFIG, "-1",
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class,
+        ProducerConfig.LINGER_MS_CONFIG, 100,
         ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true
 //        ProducerConfig.TRANSACTIONAL_ID_CONFIG, "test-s3"
       )
     );
   }
 
-  @BeforeEach
+  @BeforeAll
   void setup() throws ExecutionException, InterruptedException {
     Properties adminProps = new Properties();
     adminProps.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
 
     AdminClient adminClient = AdminClient.create(adminProps);
-    NewTopic testTopic = TopicBuilder.name("test-offset")
+    NewTopic testTopic = TopicBuilder.name(this.testTopicName)
       .compact()
       .partitions(1)
       .replicas(3)
-      .config("min.insync.replicas", "2")
-      .config("segment.ms", "10000")
+      .config(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "2")
+      .config(TopicConfig.SEGMENT_MS_CONFIG, "10000")
       .build();
 
     adminClient.createTopics(List.of(testTopic)).all().get();
@@ -70,14 +70,15 @@ class S3OffsetRepositoryTest {
     Consumer<String, Long> consumer = new KafkaConsumer<>(consumerProps);
 
     repository = new S3OffsetRepository(consumer, adminClient);
+    Thread.sleep(5_000);
   }
 
-  @AfterEach
+  @AfterAll
   void teardown() throws ExecutionException, InterruptedException {
     Properties adminProps = new Properties();
     adminProps.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
     AdminClient adminClient = AdminClient.create(adminProps);
-    adminClient.deleteTopics(Collections.singleton("test-offset")).all().get();
+    adminClient.deleteTopics(Collections.singleton(this.testTopicName)).all().get();
     adminClient.close();
   }
 
@@ -87,12 +88,12 @@ class S3OffsetRepositoryTest {
     // given
 
     try (KafkaProducer<String, Long> producer = new KafkaProducer<>(props)) {
-      for (long i = 0; i < 10_000; i++) {
-        producer.send(new ProducerRecord<>("test-offset", String.valueOf(i / 1000), i));
+      for (long i = 0; i <= 9_999; i++) {
+        producer.send(new ProducerRecord<>(this.testTopicName, String.valueOf(i / 1000), i));
       }
       producer.flush();
       Thread.sleep(Duration.ofSeconds(10L));
-      producer.send(new ProducerRecord<>("test-offset", String.valueOf(10_000), 10_000L));
+      producer.send(new ProducerRecord<>(this.testTopicName, String.valueOf(10_000), 10_000L));
 
       producer.flush();
     } catch (Exception ignored) {
@@ -100,22 +101,22 @@ class S3OffsetRepositoryTest {
     }
 
     // when
-    OffsetRecord lastOffsetRecord = repository.getLastOffsetRecord("test-offset", "10000");
+    OffsetRecord lastOffsetRecord = repository.getLastOffsetRecord(this.testTopicName, "10000");
     // then
     assertThat(lastOffsetRecord)
       .extracting(OffsetRecord::key, OffsetRecord::offset)
       .containsExactly(
         "10000",
-        10000L
+        10_000L
       );
 
   }
 
-  @DisplayName("Should get INITIAL Offset when not processed key ")
+  @DisplayName("Should get INITIAL Offset when not processed key")
   @Test
   void getLastOffsetRecord() {
      OffsetRecord lastOffset = repository.getLastOffsetRecord(
-      "s3-offset-topic",
+      this.testTopicName,
       "s3://test/2025/04/13/test.txt"
     );
 
@@ -131,11 +132,9 @@ class S3OffsetRepositoryTest {
   @Test
   void getPartitionsForTopic() {
     // given
-    String offsetTopic = "s3-offset-topic";
-
-    int partition1 = repository.getPartitionsForTopic(offsetTopic, "s3://test/2025/04/13/test.txt");
-    int partition2 = repository.getPartitionsForTopic(offsetTopic, "s3://test/2025/04/13/test.txt");
-    int partition3 = repository.getPartitionsForTopic(offsetTopic, "s3://test/2025/04/13/test.txt");
+    int partition1 = repository.getPartitionsForTopic(this.testTopicName, "s3://test/2025/04/13/test.txt");
+    int partition2 = repository.getPartitionsForTopic(this.testTopicName, "s3://test/2025/04/13/test.txt");
+    int partition3 = repository.getPartitionsForTopic(this.testTopicName, "s3://test/2025/04/13/test.txt");
 
     assertThat(List.of(partition1, partition2, partition3))
       .containsOnly(partition1);
