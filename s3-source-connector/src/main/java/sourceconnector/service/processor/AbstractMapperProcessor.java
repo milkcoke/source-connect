7ba2560885b49fefb1ee1fa169d9b3e81b3729a6
milkcoke <mbh023@gmail.com>
2025-08-30 00:59:29 +0900

    feat: Add AbstractProcessor with TemplateMethod pattern
    - Each Filter and Mapper only have to define condition and map.
    - Make simple implementation each concrete class.

---
+++ s3-source-connector/src/main/java/sourceconnector/service/processor/AbstractMapperProcessor.java
@@ -0,0 +1,15 @@
+package sourceconnector.service.processor;
+
+public abstract class AbstractMapperProcessor<Log> extends BaseProcessor<Log> {
+
+  protected abstract Log map(Log input);
+
+  @Override
+  public Log process(Log input) {
+    Log result = this.map(input);
+    if (this.next != null) {
+      return this.next.process(result);
+    }
+    return result;
+  }
+}
