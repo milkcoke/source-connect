38fa4ed2df7d79170b96b536ed05846eb75565c7
milkcoke <mbh023@gmail.com>
2025-08-30 13:46:32 +0900

    feat: Define pipeline which has multiple processor and getResult from pipeline.

---
+++ s3-source-connector/src/main/java/sourceconnector/service/pipeline/Pipeline.java
@@ -0,0 +1,9 @@
+package sourceconnector.service.pipeline;
+
+/**
+ * Pipeline consists of multiple processors and can get result
+ * @param <T> Handling type
+ */
+public interface Pipeline<T> {
+  T getResult(T input);
+}
