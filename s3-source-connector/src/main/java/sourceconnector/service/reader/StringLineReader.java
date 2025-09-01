fa9af691b305df97d3aba558ec3f3420e75646d6
milkcoke <mbh023@gmail.com>
2025-09-01 21:59:26 +0900

    feat: Add StringLineReader and FileLogPipeline
    - Implement LineReader string line by line.

---
+++ s3-source-connector/src/main/java/sourceconnector/service/reader/StringLineReader.java
@@ -0,0 +1,31 @@
+package sourceconnector.service.reader;
+
+import java.io.*;
+
+public class StringLineReader implements LineReader<String> {
+  private final LineNumberReader reader;
+
+  public StringLineReader(InputStream inputStream) {
+    reader = new LineNumberReader(new InputStreamReader(inputStream));
+  }
+
+  @Override
+  public String read() throws IOException {
+    return reader.readLine();
+  }
+
+  @Override
+  public long getLineNumber() {
+    return this.reader.getLineNumber();
+  }
+
+  @Override
+  public void setLineNumber(long lineNumber) {
+    this.reader.setLineNumber((int) lineNumber);
+  }
+
+  @Override
+  public void close() throws Exception {
+    this.reader.close();
+  }
+}
