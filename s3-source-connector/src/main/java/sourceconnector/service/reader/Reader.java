6f96af4ed24b7192f05232e8961e6906ce2f9706
milkcoke <mbh023@gmail.com>
2025-09-06 20:05:57 +0900

    docs: Add JavaDoc on the Reader
    - It's standard of Java that retruning null in read() when meets EOF.
    - So add comments about handling null as EOF.

--- s3-source-connector/src/main/java/sourceconnector/service/reader/Reader.java
+++ s3-source-connector/src/main/java/sourceconnector/service/reader/Reader.java
@@ -3,5 +3,10 @@ package sourceconnector.service.reader;
 import java.io.IOException;
 
 public interface Reader<T> extends AutoCloseable {
+  /**
+   * Read a content of type T
+   * @return A content of type T or {@code null} if end of stream has been reached
+   * @throws IOException - If an I/O error occurs
+   */
   T read() throws IOException;
 }
