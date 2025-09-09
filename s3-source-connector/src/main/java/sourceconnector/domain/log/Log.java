0caf7dcc48d8b586a9c8b92bef53f980ba93d143
milkcoke <mbh023@gmail.com>
2025-09-09 12:37:24 +0900

    feat: Add EMPTY singleton instance Log and Metadata
    - Use empty() method instance of returning null
    - Should avoid returning null.

--- s3-source-connector/src/main/java/sourceconnector/domain/log/Log.java
+++ s3-source-connector/src/main/java/sourceconnector/domain/log/Log.java
@@ -1,9 +1,21 @@
 package sourceconnector.domain.log;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Log Should have the payload and metadata
  */
 public interface Log {
   String get();
   Metadata getMetadata();
+
+  static Log empty() {
+    return Empty.INSTANCE;
+  }
+  enum Empty implements Log {
+    INSTANCE;
+    @Override public String get() { return StringUtils.EMPTY; }
+    @Override public Metadata getMetadata() { return Metadata.empty(); }
+  }
+
 }
--- s3-source-connector/src/main/java/sourceconnector/domain/log/Metadata.java
+++ s3-source-connector/src/main/java/sourceconnector/domain/log/Metadata.java
@@ -1,4 +1,12 @@
 package sourceconnector.domain.log;
 
 public interface Metadata {
+
+  static Metadata empty() {
+    return Empty.INSTANCE;
+  }
+
+  enum Empty implements Metadata {
+    INSTANCE
+  }
 }
