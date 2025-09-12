114276f3b37c37940e6fe4aa32ec526c49e05341
milkcoke <mbh023@gmail.com>
2025-09-12 09:01:38 +0900

    refactor: Update the FileLogMetadata according to the LogMetadata interface

--- s3-source-connector/src/main/java/sourceconnector/domain/log/FileLogMetadata.java
+++ s3-source-connector/src/main/java/sourceconnector/domain/log/FileLogMetadata.java
@@ -5,8 +5,12 @@ package sourceconnector.domain.log;
  * @param filePath the file path log is saved
  * @param offset offset in the log file
  */
-public record FileMetadata (
+public record FileLogMetadata(
   String filePath,
   long offset
-) implements Metadata {
+) implements LogMetadata {
+  @Override
+  public String key() {
+    return filePath;
+  }
 }
