f805b673d4b7c4215c91b9fd51dc7f01548d70c6
milkcoke <mbh023@gmail.com>
2025-09-24 01:30:19 +0900

    fix: Update the currentOffset and Reassign check logic
    - Should initialize beginning offset and end offset.
    - nextOffset should be updated by position api.

--- offset-manager-local/src/main/java/repository/LocalOffsetManager.java
+++ offset-manager-local/src/main/java/repository/LocalOffsetManager.java
@@ -37,32 +37,50 @@ public class LocalOffsetManager implements OffsetManager {
     // 4. Seek to the beginning
     consumer.seekToBeginning(topicPartitions);
 
-    // 5. Track current offsets
-    Map<TopicPartition, Long> currentOffset = new HashMap<>();
-    topicPartitions.forEach(tp -> currentOffset.put(tp, consumer.position(tp)));
+    // 5. Track next offsets to read
+    Map<TopicPartition, Long> nextOffset = consumer.beginningOffsets(topicPartitions);
+
+    // If beginning offset >= end offset, no need to poll
+    topicPartitions = topicPartitions.stream()
+        .filter(tp-> nextOffset.get(tp) < endOffsets.get(tp))
+        .toList();
 
     Set<TopicPartition> activePartitions = new HashSet<>(topicPartitions);
 
     // 6. Poll loop with dynamic unassign
     while (!activePartitions.isEmpty()) {
+      // last offset is updated automatically whenever poll is called
+      // last offset is from FetchResponse by TopicPartition
       ConsumerRecords<String, Long> records = consumer.poll(Duration.ofMillis(100));
 
+      Set<TopicPartition> scannedPartitions = new HashSet<>(activePartitions);
       for (ConsumerRecord<String, Long> record : records) {
         TopicPartition tp = new TopicPartition(record.topic(), record.partition());
-        currentOffset.put(tp, record.offset() + 1);
+        scannedPartitions.add(tp);
         this.update(record.key(), record.value());
+      }
 
+      for (TopicPartition tp : scannedPartitions) {
+        long lastOffset = consumer.position(tp);
+        nextOffset.put(tp, lastOffset);
+      }
+
+
+      for (TopicPartition tp : scannedPartitions) {
         // Reached to end offset for this partition
-        if (currentOffset.get(tp) >= endOffsets.get(tp)) {
+        if (nextOffset.get(tp) >= endOffsets.get(tp)) {
           log.info("Partition {} reached end offset {}", tp, endOffsets.get(tp));
           activePartitions.remove(tp);
-          // Re-assign only the remaining partitions
-          consumer.assign(activePartitions);
-          for (TopicPartition remainingTp : activePartitions) {
-            consumer.seek(remainingTp, currentOffset.get(remainingTp));
-          }
         }
       }
+
+      // Re-assign only the remaining partitions
+      consumer.assign(activePartitions);
+      for (TopicPartition remainingTp : activePartitions) {
+        consumer.seek(remainingTp, nextOffset.get(remainingTp));
+        log.info("Reassigned partition {} to offset {}", remainingTp, nextOffset.get(remainingTp));
+      }
+
     }
 
     this.initialized = true;
