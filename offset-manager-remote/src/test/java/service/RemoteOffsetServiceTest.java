cb5407046e2240ff733a3d93fa5ee880c3fab663
milkcoke <mbh023@gmail.com>
2025-09-26 21:26:25 +0900

    test: Add RemoteOffsetServiceTest
    - Apply mocking since RemoteOffsetService only wrapping the OffsetManager Repository.

---
+++ offset-manager-remote/src/test/java/service/RemoteOffsetServiceTest.java
@@ -0,0 +1,43 @@
+package service;
+
+import offsetmanager.domain.OffsetManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+class RemoteOffsetServiceTest {
+
+  @DisplayName("Should return offset when the key exists")
+  @Test
+  void readLastOffset() {
+    // given
+    OffsetManager mockManager = Mockito.mock(OffsetManager.class);
+    when(mockManager.findLatestOffset("existKey")).thenReturn(Optional.of(10L));
+
+    RemoteOffsetService remoteOffsetService = new RemoteOffsetService(mockManager);
+    // when
+    Optional<Long> lastOffset = remoteOffsetService.readLastOffset("existKey");
+    // then
+    assertThat(lastOffset.get()).isEqualTo(10L);
+  }
+
+  @DisplayName("Should return empty when the key does not exist")
+  @Test
+  void returnEmptyWhenNotExistKey() {
+    // given
+    OffsetManager mockManager = Mockito.mock(OffsetManager.class);
+    when(mockManager.findLatestOffset("notExistKey")).thenReturn(Optional.empty());
+
+    RemoteOffsetService remoteOffsetService = new RemoteOffsetService(mockManager);
+    // when
+    Optional<Long> lastOffset = remoteOffsetService.readLastOffset("notExistKey");
+
+    // then
+    assertThat(lastOffset).isEmpty();
+  }
+}
