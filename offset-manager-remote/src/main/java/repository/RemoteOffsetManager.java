0b385adc2be1d4ab65023ed9a010d610c5f0f5e1
milkcoke <mbh023@gmail.com>
2025-09-26 09:30:27 +0900

    feat: Update the RemoteOffsetManager constructor
    - Simplify topic scan completion condition
    - Break the initialization at the consumer position reached to all partitions

--- offset-manager-remote/src/main/java/repository/RemoteOffsetManager.java
+++ offset-manager-remote/src/main/java/repository/RemoteOffsetManager.java
@@ -45,44 +45,17 @@ public class RemoteOffsetManager implements OffsetManager {
     // 4. Seek to the beginning
     consumer.seekToBeginning(topicPartitions);
 
-    // 5. Track next offsets to read
-    Map<TopicPartition, Long> earliestOffsets = consumer.beginningOffsets(topicPartitions);
+    boolean completeScan = endOffsets.entrySet().stream()
+      .allMatch(endOffset -> consumer.position(endOffset.getKey()) >= endOffset.getValue());
 
-    // Initialize scanned partitions which are already scanned to the end
-    Set<TopicPartition> scannedPartitions = topicPartitions.stream()
-      .filter(tp -> earliestOffsets.get(tp) >= endOffsets.get(tp))
-      .collect(Collectors.toSet());
-
-    // 6. Scan all partitions until all partitions reach end offsets
-    while (topicPartitions.size() != scannedPartitions.size()) {
+    while(!completeScan) {
       ConsumerRecords<String, Long> records = consumer.poll(Duration.ofMillis(100));
-
-      // 6-1. Process records and update offset store
-      for (ConsumerRecord<String, Long> record : records) {
-        this.update(record.key(), record.value());
-      }
-
-      // 6-2 Only check the partitions that are not fully scanned yet
-      // FIXME: If Transaction markers exists between earliestOffsets and endOffsets, it could not be detected.
-      Set<TopicPartition> retrievedPartitions = records.partitions();
-      for (TopicPartition tp : retrievedPartitions) {
-        long currentLastOffset = consumer.position(tp);
-
-          // 6-3. Pause the fetch about the partitions that reached end offsets
-        if (currentLastOffset >= endOffsets.get(tp)) {
-          log.info("Partition {} reached end offset {}", tp, endOffsets.get(tp));
-          consumer.pause(Collections.singleton(tp));
-          scannedPartitions.add(tp);
-        }
-      }
+      // 5-1. Process records and update offset store
+      records.forEach(record -> this.update(record.key(), record.value()));
+      // 5-2 Only check the partitions that are not fully scanned yet
+      completeScan = endOffsets.entrySet().stream()
+        .allMatch(endOffset -> consumer.position(endOffset.getKey()) >= endOffset.getValue());
     }
-
-      // TODO: Refactor this
-      //    (1) pause and resume approach << pause protects from sending fetch request to the partition
-      //     (2) Remove a partition from assignment and Reassign the all partitions at the end.
-
-    consumer.resume(scannedPartitions);
-    // resume all partitions
     log.info("All partitions reached end offsets, finished the initializing.");
     this.executorService.submit(this::runUpdate);
   }
