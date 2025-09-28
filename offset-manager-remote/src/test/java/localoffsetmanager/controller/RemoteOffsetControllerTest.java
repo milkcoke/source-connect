80e7ee740962e4806deff0c11db2c0493c11e271
milkcoke <mbh023@gmail.com>
2025-09-28 22:46:51 +0900

    test: Add RemoteOffsetControllerTest

--- offset-manager-remote/src/test/java/localoffsetmanager/controller/RemoteOffsetControllerTest.java
+++ offset-manager-remote/src/test/java/localoffsetmanager/controller/RemoteOffsetControllerTest.java
@@ -1,17 +1,27 @@
 package localoffsetmanager.controller;
 
+import localoffsetmanager.exception.OffsetNotFoundException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class RemoteOffsetControllerTest extends ControllerTestSupport {
 
+  @DisplayName("Should throw 404 Not Found when the offset record does not exist")
   @Test
-  void getLastOffsetRecord() throws Exception {
+  void OffsetNotFoundTest() throws Exception {
+    // given
+    when(remoteOffsetService.readLastOffset("notExistKey"))
+      .thenThrow(new OffsetNotFoundException("notExistKey"));
+
     String responsePayload = mockMvc.perform(
+      // when
       MockMvcRequestBuilders.get("/v1/offset-records")
         .param("key", "notExistKey")
     )
@@ -20,12 +30,14 @@ class RemoteOffsetControllerTest extends ControllerTestSupport {
       .getResponse()
       .getContentAsString();
 
+    // then
     JSONAssert.assertEquals(
       responsePayload,
       """
         {
-          "key": "notExistKey",
-          "message": "Not Found Last Offset Record by key: notExistKey",
+          "statusCode": 404,
+          "type": "OFFSET_NOT_FOUND",
+          "message": "Offset not found for key: notExistKey"
         }
         """,
       JSONCompareMode.STRICT
@@ -33,7 +45,35 @@ class RemoteOffsetControllerTest extends ControllerTestSupport {
 
   }
 
+  @DisplayName("Should return 400 Bad Request when the key parameter is invalid")
   @Test
-  void getLastOffsetRecords() {
+  void invalidKeyRequestTest() throws Exception {
+    // given
+    String responsePayload = mockMvc.perform(
+      // when
+      MockMvcRequestBuilders.get("/v1/offset-records")
+       .param("key", "a")
+    )
+      .andDo(print())
+      .andExpect(status().isBadRequest())
+      .andReturn()
+      .getResponse()
+      .getContentAsString();
+
+    JSONAssert.assertEquals(
+      responsePayload,
+      """
+        {
+          "statusCode": 400,
+          "type": "INVALID_PARAMETER",
+          "message": "Invalid parameters",
+          "properties":{
+            "key": "Key must be at least 5 length"
+          }
+        }
+        """,
+      JSONCompareMode.STRICT
+    );
   }
+
 }
