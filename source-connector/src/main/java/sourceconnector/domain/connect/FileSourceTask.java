0316215ec0895db311445dd4bdb3572976815a70
milkcoke <mbh023@gmail.com>
2025-10-09 13:21:37 +0900

    feat: Define FileTaskAssignor

--- source-connector/src/main/java/sourceconnector/domain/connect/FileSourceTask.java
+++ source-connector/src/main/java/sourceconnector/domain/connect/FileSourceTask.java
@@ -1,13 +1,32 @@
 package sourceconnector.domain.connect;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import sourceconnector.repository.file.FileRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class FileSourceTask implements Task<FileProcessingResult>{
-  private final FileProcessingResult result;
+  private final String id;
+  @Getter
+  private final int index;
+  private final FileRepository fileRepository;
+
+  // visible for test
+  @Getter(AccessLevel.PACKAGE)
+  private final List<String> filePaths = new ArrayList<>();
+  private final FileProcessingResult result = new FileProcessingResult();
 
   @Override
   public FileProcessingResult call() throws Exception {
     return null;
+  }
+
+  @Override
+  public void assign(List<String> filePathList) {
+    filePaths.addAll(filePathList);
   }
 }
---
+++ source-connector/src/test/java/sourceconnector/domain/connect/FileTaskAssignorTest.java
@@ -0,0 +1,78 @@
+package sourceconnector.domain.connect;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import sourceconnector.repository.file.LocalFileRepository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileTaskAssignorTest {
+
+  @DisplayName("First task has the number of 6 file paths when task 5 and file paths 30 provided")
+  @Test
+  void assignedFileCountTest() {
+
+    // given
+    List<String> filePaths = new ArrayList<>();
+    for (int i = 0; i < 30; i++) {
+      filePaths.add("file-" + i);
+    }
+    TaskAssignor taskAssignor = new FileTaskAssignor(filePaths, 5);
+    FileSourceTask task0 = new FileSourceTask("Task-0", 0, new LocalFileRepository());
+
+    // when
+    taskAssignor.assign(List.of(task0));
+
+    // then
+    assertThat(task0.getFilePaths())
+      .hasSize(6);
+  }
+
+  @DisplayName("First and second task have the number of 8 file paths when task 4 and file paths 30 provided")
+  @Test
+  void assignedFileCountTest2() {
+    // given
+    List<String> filePaths = new ArrayList<>();
+    for (int i = 0; i < 30; i++) {
+      filePaths.add("file-" + i);
+    }
+    TaskAssignor taskAssignor = new FileTaskAssignor(filePaths, 4);
+    FileSourceTask task0 = new FileSourceTask("Task-0", 0, new LocalFileRepository());
+    FileSourceTask task1 =  new FileSourceTask("Task-1", 1, new LocalFileRepository());
+    FileSourceTask task2 =  new FileSourceTask("Task-2", 2, new LocalFileRepository());
+    FileSourceTask task3 =   new FileSourceTask("Task-3", 3, new LocalFileRepository());
+
+    Collection<Task<FileProcessingResult>> tasks = List.of(task0, task1, task2, task3);
+
+    // when
+    taskAssignor.assign(tasks);
+
+    // then
+    assertAll(
+      ()->assertThat(task0.getFilePaths()).hasSize(8),
+      ()->assertThat(task1.getFilePaths()).hasSize(8),
+      ()->assertThat(task2.getFilePaths()).hasSize(7),
+      ()->assertThat(task3.getFilePaths()).hasSize(7)
+    );
+  }
+
+  @DisplayName("Can assign even though file path has no element")
+  @Test
+  void emptyFilePathAssignTest() {
+    // given
+    TaskAssignor taskAssignor = new FileTaskAssignor(Collections.emptyList(), 1);
+    Collection<Task<FileProcessingResult>> tasks = List.of(
+      new FileSourceTask("Task-0", 0, new LocalFileRepository())
+    );
+
+    // when then
+    assertDoesNotThrow(() -> taskAssignor.assign(tasks));
+  }
+
+}
