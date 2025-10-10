a13161f9dfbba817ae530c30861cb4a676e0d749
milkcoke <mbh023@gmail.com>
2025-10-11 01:52:51 +0900

    refactor: Add addSuccessCount, FailureCount methods on the FileProcessingResult
    - Express the intention of the operation instead of setter.

--- source-connector/src/main/java/sourceconnector/domain/connect/FileProcessingResult.java
+++ source-connector/src/main/java/sourceconnector/domain/connect/FileProcessingResult.java
@@ -3,9 +3,19 @@ package sourceconnector.domain.connect;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
 public class FileProcessingResult {
+  @Setter
   private int totalCount = 0;
   private int successCount = 0;
   private int failureCount = 0;
+
+  public void addSuccessCount() {
+    successCount++;
+  }
+
+  public void addFailureCount() {
+    failureCount++;
+  }
+
 }
