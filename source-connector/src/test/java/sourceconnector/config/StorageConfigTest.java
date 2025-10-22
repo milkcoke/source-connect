57cc6350c5a0697ab81604d7a7d62883f672149d
milkcoke <mbh023@gmail.com>
2025-10-23 02:44:45 +0900

    test: Add StorageConfigTest
    - Test mapping yaml to POJO StorageConfig class without springboot dependency.

--- source-connector/src/test/java/sourceconnector/config/StorageConfigTest.java
+++ source-connector/src/test/java/sourceconnector/config/StorageConfigTest.java
@@ -1,24 +1,56 @@
 package sourceconnector.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.core.io.ByteArrayResource;
+import sourceconnector.config.StorageConfig.StorageType;
 
+import java.io.IOException;
+import java.util.Map;
 
-@SpringBootTest(classes = StorageConfig.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
 class StorageConfigTest {
 
-  @Autowired
-  private StorageConfig storageConfig;
-
-  @DisplayName("Display")
+  @DisplayName("Should get storage mapping according to yaml string")
   @Test
-  void printTest() {
+  void storageConfigMappingTest() throws IOException {
     // given
-    System.out.printf("storageConfig=%s%n", storageConfig);
+    Map<String, Object> map = getStringObjectMap("""
+    app:
+      storage:
+        type: s3
+        paths:
+          - s3://my-bucket/foo
+          - s3://my-bucket/bar
+    """);
+
+    Binder binder = new Binder(new MapConfigurationPropertySource(map));
     // when
+    StorageConfig config = binder.bind("app.storage", StorageConfig.class).get();
 
     // then
+    assertThat(config.type()).isEqualTo(StorageType.S3);
+    assertThat(config.filters()).isNullOrEmpty();
+    assertThat(config.paths()).containsExactlyInAnyOrder(
+      "s3://my-bucket/foo",
+      "s3://my-bucket/bar"
+    );
+  }
+
+  @SuppressWarnings("unchecked")
+  @NotNull
+  private Map<String, Object> getStringObjectMap(String yamlStr) throws IOException {
+
+    var loader = new YamlPropertySourceLoader();
+    var resource = new ByteArrayResource(yamlStr.getBytes());
+    var propertySource = loader.load("test", resource).getFirst();
+
+    // Convert PropertySource to MapConfigurationPropertySource for Binder
+    return (Map<String, Object>) propertySource.getSource();
   }
 }
