174bd2c9eac6104f5ede8500a620fcff85adad3d
milkcoke <mbh023@gmail.com>
2025-11-02 16:09:44 +0900

    test: Add Url Parsing test on the AppConfigTest

--- source-connector/src/test/java/sourceconnector/config/AppConfigTest.java
+++ source-connector/src/test/java/sourceconnector/config/AppConfigTest.java
@@ -2,16 +2,20 @@ package sourceconnector.config;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.bind.BindException;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
 import org.springframework.boot.origin.OriginTrackedValue;
 import sourceconnector.config.util.YamlTestUtils;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AppConfigTest {
@@ -59,4 +63,38 @@ class AppConfigTest {
       .hasStackTraceContaining("offsetManagerBaseUrl is required");
   }
 
+  @DisplayName("Should throw BindException when invalid Url provided")
+  @Test
+  void invalidUrlFailTest() throws IOException {
+    // given
+    Map<String, Object> map = YamlTestUtils.getStringObjectMap("""
+      app:
+        workerCount: 1
+        taskCount: 2
+        offsetManagerBaseUrl: localhost:8080
+      """);
+    Binder binder = new Binder(new MapConfigurationPropertySource(map));
+    // when then
+    assertThatThrownBy(()-> binder.bind("app", AppConfig.class).get())
+      .isInstanceOf(BindException.class);
+
+  }
+
+  @DisplayName("offsetManagerBaseUrl should be parsed as URL")
+  @Test
+  void urlParseTest() throws IOException {
+    // given
+    Map<String, Object> map = YamlTestUtils.getStringObjectMap("""
+      app:
+        workerCount: 1
+        taskCount: 2
+        offsetManagerBaseUrl: http://localhost:8080
+      """);
+    Binder binder = new Binder(new MapConfigurationPropertySource(map));
+    // when
+    AppConfig appConfig = binder.bind("app", AppConfig.class).get();
+
+    // then
+    assertThat(appConfig.offsetManagerBaseUrl()).isInstanceOf(URL.class);
+  }
 }
