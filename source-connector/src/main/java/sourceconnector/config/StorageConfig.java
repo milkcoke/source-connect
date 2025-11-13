fccbd5a52e07c3e06cc9443bd80931f7e88581db
milkcoke <mbh023@gmail.com>
2025-11-13 21:03:24 +0900

    feat: Define a fileKeyFactory method on StorageType
    - In project, FileKey is used as domain.
    - So converting from String to FileKey is to be supported according to StorageType.

--- source-connector/src/main/java/sourceconnector/config/StorageConfig.java
+++ source-connector/src/main/java/sourceconnector/config/StorageConfig.java
@@ -1,18 +1,32 @@
 package sourceconnector.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import sourceconnector.domain.file.FileKey;
+import sourceconnector.domain.file.LocalFileKey;
+import sourceconnector.domain.file.S3FileKey;
+import sourceconnector.domain.file.S3Uri;
 
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 
 @ConfigurationProperties(prefix = "source.storage")
 public record StorageConfig(
   StorageType type,
   List<String> paths
 ) {
+  @RequiredArgsConstructor
   public enum StorageType {
-    LOCAL,
-    S3
+    LOCAL(path -> LocalFileKey.from(Path.of(path))),
+    S3(path -> S3FileKey.from(S3Uri.from(path)));
+
+    private final Function<String, FileKey> fileKeyFactory;
+
+    public FileKey toFileKey(String path) {
+      return fileKeyFactory.apply(path);
+    }
   }
   public StorageConfig {
     Objects.requireNonNull(type, "storage type is required");
