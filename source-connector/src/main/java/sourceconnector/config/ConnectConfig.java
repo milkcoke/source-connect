6aae0c7e7f0209b0b055da7d833ab52f8668a706
milkcoke <mbh023@gmail.com>
2025-12-02 09:04:27 +0900

    refactor: Rename AppConfig to ConnectConfig
    - taskCount and workerCount bound to the Connect only.
    - AppConfig is so general name.

--- source-connector/src/main/java/sourceconnector/config/ConnectConfig.java
+++ source-connector/src/main/java/sourceconnector/config/ConnectConfig.java
@@ -2,18 +2,13 @@ package sourceconnector.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.net.URL;
-import java.util.Objects;
-
-@ConfigurationProperties(prefix = "app")
-public record AppConfig(
+@ConfigurationProperties(prefix = "connect")
+public record ConnectConfig(
   int workerCount,
-  int taskCount,
-  URL offsetManagerBaseUrl
+  int taskCount
 ) {
-  public AppConfig {
+  public ConnectConfig {
     if (workerCount < 1) throw new IllegalArgumentException("workerCount must be >= 1");
     if (taskCount < workerCount) throw new IllegalArgumentException("taskCount must be >= workerCount");
-    Objects.requireNonNull(offsetManagerBaseUrl, "offsetManagerBaseUrl is required");
   }
 }
