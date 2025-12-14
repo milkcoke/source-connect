268c3cf4c996d946f951be2ed53426646e6fca52
milkcoke <mbh023@gmail.com>
2025-12-14 16:55:10 +0900

    build: Update offset-manager build.gradle
    - Update SpringBoot version from 3.5 to 4.0

--- offset-manager/src/main/java/offsetmanager/config/KafkaConfig.java
+++ offset-manager/src/main/java/offsetmanager/config/KafkaConfig.java
@@ -3,7 +3,7 @@ package offsetmanager.config;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +16,7 @@ public class KafkaConfig {
   public KafkaConsumer<String, Long> consumer(KafkaProperties kafkaProperties) {
     Properties properties = new Properties();
     properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, String.join(",", kafkaProperties.getBootstrapServers()));
-    properties.putAll(kafkaProperties.getConsumer().buildProperties(null));
+    properties.putAll(kafkaProperties.getConsumer().buildProperties());
     return new KafkaConsumer<>(properties);
   }
 
@@ -24,7 +24,7 @@ public class KafkaConfig {
   public AdminClient adminClient(KafkaProperties kafkaProperties) {
     Properties properties = new Properties();
     properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, String.join(",", kafkaProperties.getBootstrapServers()));
-    properties.putAll(kafkaProperties.getAdmin().buildProperties(null));
+    properties.putAll(kafkaProperties.getAdmin().buildProperties());
     return AdminClient.create(properties);
   }
 }
