04988001412fc5448f363e5c01f69319c28e730d
milkcoke <mbh023@gmail.com>
2025-12-23 20:08:08 +0900

    fix: Apply @TempDir on the LocalFileRepositoryTest
    - Should create test file without file system dependencies.

--- source-connector/src/test/java/sourceconnector/repository/file/LocalFileRepositoryTest.java
+++ source-connector/src/test/java/sourceconnector/repository/file/LocalFileRepositoryTest.java
@@ -1,23 +1,27 @@
 package sourceconnector.repository.file;
 
+import offsetmanager.domain.file.FileKey;
+import offsetmanager.domain.file.LocalFileKey;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import offsetmanager.domain.file.FileKey;
-import offsetmanager.domain.file.LocalFileKey;
+import org.junit.jupiter.api.io.TempDir;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LocalFileRepositoryTest {
 
   private final FileRepository fileRepository = new LocalFileRepository();
+
+  @TempDir
+  Path tempDir;
+
   @DisplayName("Should throw not found remoteoffsetmanager.exception when file does not exist")
   @Test
   void shouldThrowNoSuchFileException() {
@@ -31,27 +35,23 @@ class LocalFileRepositoryTest {
   @Test
   void getInputStream() throws IOException {
     // given
-    Path path = Path.of("src/test/resources/test-file.txt");
-    File file = path.toFile();
-    file.createNewFile();
+    Path tempFilePath = Files.createTempFile(tempDir, null, null);
     // when
-    FileKey fileKey = LocalFileKey.from(path);
+    FileKey fileKey = LocalFileKey.from(tempFilePath);
     InputStream inputStream = this.fileRepository.getFile(fileKey);
     // then
     assertThat(inputStream).isNotNull();
-    file.delete();
   }
 
   @DisplayName("Should get inpustream when file exists in local file system")
   @Test
   void getDownloadDirectoryFileInputStream() throws IOException {
     // given
-    File sampleFile = Paths.get(System.getProperty("user.home"), "Downloads", "sample.txt").toFile();
+    Path tempFilePath = Files.createTempFile(tempDir, null, null);
     // when
-    sampleFile.createNewFile();
-    FileKey fileKey = LocalFileKey.from(sampleFile.toPath());
-    InputStream inputStream = this.fileRepository.getFile(fileKey);
+    FileKey fileKey = LocalFileKey.from(tempFilePath);
     // then
-    sampleFile.delete();
+    InputStream inputStream = this.fileRepository.getFile(fileKey);
+    assertThat(inputStream).isNotNull();
   }
 }
