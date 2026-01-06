f8dbdaabce284ce15dc4c7c72cc55853d03c916e
milkcoke <mbh023@gmail.com>
2026-01-06 23:54:26 +0900

    build: Update the settings.gradle.kts
    - Remove unnecessary plugin.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,6 @@ pluginManagement {
     kotlin("jvm") version "2.3.0"
   }
 }
-plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
 rootProject.name = "source-connect"
 include(
   "kafka-streams-connect",
